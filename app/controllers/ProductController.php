1b36246e0df29b4cb0985f32e5de0e8441045acf
sohel <sohelcse1999@gmail.com>
2019-03-24 22:43:40 +0600

    .htaccess file add and configuer

---
+++ app/controllers/ProductController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MD.SOHEL
+ * Date: 3/24/2019
+ * Time: 12:20 PM
+ */
+
+namespace App\controllers;
+
+
+class ProductController
+{
+
+}
---
+++ app/controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MD.SOHEL
+ * Date: 3/22/2019
+ * Time: 8:00 PM
+ */
+
+namespace App\controllers;
+
+
+class UserController
+{
+
+}
