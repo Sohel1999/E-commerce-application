fe1b7cc9343c428b0668680a6ac3820d6083b884
sohel <sohelcse1999@gmail.com>
2019-03-25 14:09:23 +0600

    configure illument database

--- index.php
+++ index.php
@@ -1,15 +1,29 @@
 <?php
 
-
 use Phroute\Phroute\Exception\HttpMethodNotAllowedException;
 use Phroute\Phroute\Exception\HttpRouteNotFoundException;
 use Phroute\Phroute\RouteCollector;
 use Phroute\Phroute\RouteParser;
 use Phroute\Phroute\Dispatcher ;
+use Illuminate\Database\Capsule\Manager as Capsule;
 require_once 'vendor/autoload.php';
 $router=new RouteCollector(new RouteParser());
 include_once __DIR__.'/routers.php';
-
+$capsule=new Capsule();
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'e-commerce',
+    'username'  => 'root',
+    'password'  => '',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+$users=Capsule::table('users')->first();
+var_dump($users->username); die();
 
 $dispatcher = new Dispatcher($router->getData());
 
